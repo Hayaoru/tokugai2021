e8d6c03b1eafa64e231a5b7227930088d843789e
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2021-03-01 15:02:55 +0900

    ER図追記

--- db/migrate/20210217174757_create_infos.rb
+++ db/migrate/20210217174757_create_infos.rb
@@ -1,7 +1,11 @@
 class CreateInfos < ActiveRecord::Migration[6.0]
   def change
     create_table :infos do |t|
-
+      t.string :title,     null: false, default: ""
+      t.text :info,            null: false
+      t.integer :category_id,  null: false
+      
+      t.references :user,      foreign_key: true
       t.timestamps
     end
   end
